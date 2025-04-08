cbb4b42245a180980e3a658a185fbfe9a1a3ae70
Manda3312 <ikotokelymanda@gmail.com>
2025-04-08 19:26:12 +0200

    Update Util.java

--- src/main/java/Util/Util.java
+++ src/main/java/Util/Util.java
@@ -4,9 +4,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/ETU003309?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://172.80.237.53/db_s2_ETU003312?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";  // Correct variable name casing
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "hvs3CCHV";
 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         // Load MySQL JDBC Driver
